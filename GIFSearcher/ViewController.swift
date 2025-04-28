d7e9c6b3ab720ddf0aaad50cb1584952de3fcd18
Maksim Li <maximlee.ml@gmail.com>
2025-04-28 23:07:02 +0300

    Initial Commit

---
+++ GIFSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GIFSearcher
+//
+//  Created by Maksim Li on 28/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
