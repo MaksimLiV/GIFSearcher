92d28adc169d7dd26ba690e044aca53cf96ef972
Maksim Li <maximlee.ml@gmail.com>
2025-04-29 22:56:00 +0300

    new chages

---
+++ GIFSearcher/Utils/Extensions/UIImageViewGif.swift
@@ -0,0 +1,83 @@
+//
+//  UIImageViewGif.swift
+//  GIFSearcher
+//
+//  Created by Maksim Li on 29/04/2025.
+//
+
+import UIKit
+
+extension UIImageView {
+    private static var taskKey = "GIFDownloadTask"
+    
+    private var currentTask: URLSessionDataTask? {
+        get {
+            return objc_getAssociatedObject(self, &UIImageView.taskKey) as? URLSessionDataTask
+        }
+        set {
+            objc_setAssociatedObject(self, &UIImageView.taskKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    func setGifFromURL(_ url: URL, placeholderImage: UIImage? = nil) {
+
+        self.image = placeholderImage
+        cancelCurrentTask()
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self,
+                  let data = data,
+                  error == nil,
+                  let source = CGImageSourceCreateWithData(data as CFData, nil) else {
+                return
+            }
+
+            let count = CGImageSourceGetCount(source)
+            
+            // Если только один кадр, это обычная картинка
+            if count == 1, let cgImage = CGImageSourceCreateImageAtIndex(source, 0, nil) {
+                DispatchQueue.main.async {
+                    self.image = UIImage(cgImage: cgImage)
+                }
+                return
+            }
+            
+            var images = [UIImage]()
+            var totalDuration: TimeInterval = 0
+            
+            for i in 0..<count {
+                if let cgImage = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                    let frameProperties = CGImageSourceCopyPropertiesAtIndex(source, i, nil) as? [String: Any]
+                    let gifProperties = frameProperties?[kCGImagePropertyGIFDictionary as String] as? [String: Any]
+                    
+                    var frameDuration: TimeInterval = 0.1
+                    if let delayTime = gifProperties?[kCGImagePropertyGIFDelayTime as String] as? TimeInterval {
+                        frameDuration = delayTime
+                    }
+                    
+                    totalDuration += frameDuration
+                    images.append(UIImage(cgImage: cgImage))
+                }
+            }
+            
+            if !images.isEmpty {
+                DispatchQueue.main.async {
+                    self.image = UIImage.animatedImage(with: images, duration: totalDuration)
+                }
+            }
+        }
+        
+        currentTask = task
+        task.resume()
+    }
+    
+    private func cancelCurrentTask() {
+        currentTask?.cancel()
+        currentTask = nil
+    }
+    
+    func prepareForReuse() {
+        cancelCurrentTask()
+        image = nil
+    }
+}
---
+++ GIFSearcher/Utils/Extensions/UIViewControllerAlert.swift.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewControllerAlert.swift.swift
+//  GIFSearcher
+//
+//  Created by Maksim Li on 29/04/2025.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(title: String, message: String, actionTitle: String = "OK", completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: actionTitle, style: .default) { _ in
+            completion?()
+        }
+        
+        alert.addAction(action)
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func showErrorAlert(_ error: Error, completion: (() -> Void)? = nil) {
+        showAlert(title: "Ошибка", message: error.localizedDescription, completion: completion)
+    }
+}
