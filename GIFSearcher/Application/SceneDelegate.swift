4d59fd1063bef8399e3c5be8d9ae1afcc314736a
Maksim Li <maximlee.ml@gmail.com>
2025-05-02 01:04:49 +0300

    new changes

---
+++ GIFSearcher/Application/SceneDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  SceneDelegate.swift
+//  GIFSearcher
+//
+//  Created by Maksim Li on 28/04/2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        
+        let searchViewController = GIFSearchViewController()
+        let navigationController = UINavigationController(rootViewController: searchViewController)
+        
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+        NetworkMonitor.shared.stopMonitoring()
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
