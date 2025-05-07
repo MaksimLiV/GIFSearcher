6576eae71ae06ff530e211df3447b7fe05001310
Maksim Li <maximlee.ml@gmail.com>
2025-05-07 22:09:17 +0300

    Changes related to API

--- GIFSearcher/Config/APIConfig.swift
+++ GIFSearcher/Config/APIConfig.swift
@@ -8,10 +8,6 @@
 import Foundation
 
 struct APIConfig {
-#if DEBUG
     static let apiKey = SecretAPIKey.key
-#else
-    static let apiKey = PublicAPIKey.key
-#endif
     static let baseURL = "https://api.giphy.com/v1"
 }
