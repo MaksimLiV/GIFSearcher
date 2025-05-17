70a59a08445475e851756dfbf66f9caf52dbb447
Maksim Li <maximlee.ml@gmail.com>
2025-05-17 23:04:02 +0300

    changed from Alamofire to URLsesstion

--- GIFSearcher/Services/NetworkService/NetworkService.swift
+++ GIFSearcher/Services/NetworkService/NetworkService.swift
@@ -6,16 +6,15 @@
 //
 
 import Foundation
-import Alamofire
 
 protocol NetworkServiceProtocol {
     func request<T: Decodable>(endpoint: Endpoint, completion: @escaping (Result<T, NetworkError>) -> Void)
 }
 
 class NetworkService: NetworkServiceProtocol {
-    private let session: Session
+    private let session: URLSession
     
-    init(session: Session = .default) {
+    init(session: URLSession = .shared) {
         self.session = session
     }
     
@@ -26,44 +25,40 @@ class NetworkService: NetworkServiceProtocol {
             return
         }
         
-        guard let urlRequest = endpoint.makeURLRequest() else {
+        guard let request = endpoint.makeURLRequest() else {
             completion(.failure(.invalidURL))
             return
         }
         
-        session.request(urlRequest)
-            .validate(statusCode: 200..<300)
-            .responseDecodable(of: T.self) { response in
-                switch response.result {
-                case .success(let decodedObject):
-                    completion(.success(decodedObject))
-                    
-                case .failure(let error):
-                    if let urlError = error.underlyingError as? URLError {
-                        completion(.failure(.unknown(urlError)))
-                        return
-                    }
-                    
-                    if let afError = error.asAFError {
-                        switch afError {
-                        case .invalidURL:
-                            completion(.failure(.invalidURL))
-                        case .responseValidationFailed(let reason):
-                            if case .unacceptableStatusCode(let code) = reason {
-                                completion(.failure(.serverError(code)))
-                            } else {
-                                completion(.failure(.invalidResponse))
-                            }
-                        case .responseSerializationFailed:
-                            completion(.failure(.decodingError(error)))
-                        default:
-                            completion(.failure(.unknown(error)))
-                        }
-                        return
-                    }
-                    
-                    completion(.failure(.unknown(error)))
-                }
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.unknown(error)))
+                return
             }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(.serverError(httpResponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decodedObject = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedObject))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }
+        
+        task.resume()
     }
 }
